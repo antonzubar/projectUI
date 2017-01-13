488e273a202e2613dfbb9be1b85559b91868c20b
antonzubar <anton-zubar@yandex.ru>
2017-01-13 17:16:40 +0300

    Add files via upload

--- test1.py
+++ test1.py
@@ -3,51 +3,105 @@ from tkinter import ttk
 import test2
 
 root = Tk()
-
 root.resizable(False, False)
 root.title('TIAA Automation')
+main_orange = '#%02x%02x%02x' % (255, 184, 61)
+
 n = ttk.Notebook(root)
-f1 = ttk.Frame(n)   # first page, which would get widgets gridded into it
-f2 = ttk.Frame(n)   # second page
-n.add(f1, text='One')
-n.add(f2, text='Two')
-n.place(x=0,y=0)
-horiz_placemnt, vert_placemnt = test2.center(root)
+f1 = ttk.Frame(n)  # first page, which would get widgets gridded into it
+f2 = ttk.Frame(n)  # second page
+n.add(f1, text='Manage Tests')
+n.add(f2, text='Manage Data')
+n.place(x=0, y=0)
+
+# Create style of tabs
+ttk.Style().configure("TNotebook", background=main_orange)
+ttk.Style().map("TNotebook.Tab", background=[('selected', "grey")], foreground=[("selected", "grey")])
+ttk.Style().configure("TNotebook.Tab", background="white", foreground="DarkGoldenrod4")
+
+horiz_placemnt, vert_placemnt = test2.center(root, "root")
 
 im = PhotoImage(file='D:/testing/soft/2.gif')
-label1 = Label(root)
-label1["image"] = im
-label1.place(x=0, y=0)
+label1 = Label(f1, image=im)
+label2 = Label(f2, image=im)
+label1.pack()
+label2.pack()
 
-listbox1 = Listbox(root, width=42, height=20, selectmode=EXTENDED)
+listbox1 = Listbox(f1, selectmode=EXTENDED)
+settings = open('D:/testing/soft/Settings.ini', 'r').readlines()
+if settings != []:
+    a = os.listdir(settings[0])
+    for i in a:
+        if i == ".cache" or i == "__pycache__" or i == "rep" or i == "BaseTestCase.py" or i == "ConfigParser.py" or i == "GetPage.py":
+            continue
+        elif i.endswith('.py'):
+            listbox1.insert(END, i)
+    listbox1.place(x=2, y=0, height=vert_placemnt * 0.87, width=horiz_placemnt * 0.4)
+    scrollbar = Scrollbar(listbox1)
+    scrollbar.place(x=horiz_placemnt * 0.4 - 19, y=-1, height=vert_placemnt * 0.87)
+    scrollbar['command'] = listbox1.yview
+    listbox1['yscrollcommand'] = scrollbar.set
+else:
+    print("bla")
+    settings = ''
 
-button_color = '#%02x%02x%02x' % (255, 184, 61)
+# ------------Button's style variables-----------------------------------------------------------------
 button_font = 'Anago 12'
-button_menu_width = 193  # 20
-button_menu_height = 51  # 2
+button_menu_width = 150  # 193
+button_menu_height = 25  # 51
+border_value = 0
+# -----------------------------------------------------------------------------------------------------
 
-button1_image = PhotoImage(file='D:/testing/soft/run_test.gif')
-button1 = Button(root, width=button_menu_width, height=button_menu_height, bg=button_color,
-                 font=button_font, image=button1_image,
-                 command=lambda: test2.run_test(listbox1))  # height = 3 == 65 pixels
-button1.place(x=horiz_placemnt * 0.55, y=65)
+# ------------Button's section-------------------------------------------------------------------------
 
-button2_image = PhotoImage(file='D:/testing/soft/generate_report.gif')
-button2 = Button(root, width=button_menu_width, height=button_menu_height, bg=button_color,
-                 fg="white", image=button2_image,
-                 command=test2.generate_report, font=button_font)
-button2.place(x=horiz_placemnt * 0.55, y=121)
+# RUN TESTS button desc
+run_tests_image = PhotoImage(file='D:/testing/soft/run_test.gif')
+run_tests_image_lighted = PhotoImage(file='D:/testing/soft/run_test_lighted.gif')
+run_tests_button = Button(f1, width=button_menu_width, height=button_menu_height, bd=border_value,
+                          font=button_font, image=run_tests_image,
+                          command=lambda: test2.run_test(listbox1, settings, run_tests_button, horiz_placemnt))  # height = 3 == 65 pixels
+run_tests_button.place(x=horiz_placemnt * 0.46, y=145)
+run_tests_button.bind("<Enter>", lambda a: run_tests_button.configure(image=run_tests_image_lighted))
+run_tests_button.bind("<Leave>", lambda a: run_tests_button.configure(image=run_tests_image))
 
-button3 = Button(root, width=button_menu_width, height=button_menu_height, bg=button_color,
-                 fg="white",
-                 command=test2.open_last_report, font=button_font)
-button3.place(x=horiz_placemnt * 0.55, y=177)
-button3_image = PhotoImage(file='D:/testing/soft/open_last_report.gif')
-button3["image"] = button3_image
+# GENERATE REPORT button desc
+generate_report_image = PhotoImage(file='D:/testing/soft/generate_report.gif')
+generate_report_image_lighted = PhotoImage(file='D:/testing/soft/generate_report_lighted.gif')
+generate_report_button = Button(f1, width=button_menu_width, height=button_menu_height, bd=border_value,
+                                fg="white", image=generate_report_image,
+                                command=lambda: test2.generate_report, font=button_font)
+generate_report_button.place(x=horiz_placemnt * 0.702, y=114)
+generate_report_button.bind("<Enter>", lambda a: generate_report_button.configure(image=generate_report_image_lighted))
+generate_report_button.bind("<Leave>", lambda a: generate_report_button.configure(image=generate_report_image))
 
-button4 = Button(root, text="Browse Your Tests", width=35, height=1,
-                 command=lambda: test2.browsecsv(listbox1, vert_placemnt))
-button4.place(x=0, y=vert_placemnt * 0.925)
+# OPEN LAST REPORT button desc
+open_last_report_image = PhotoImage(file='D:/testing/soft/open_last_report.gif')
+open_last_report_image_lighted = PhotoImage(file='D:/testing/soft/open_last_report_lighted.gif')
+open_last_report_button = Button(f1, width=button_menu_width, height=button_menu_height, bd=border_value,
+                                 fg="white", image=open_last_report_image,
+                                 command=lambda: test2.open_last_report, font=button_font)
+open_last_report_button.place(x=horiz_placemnt * 0.702, y=145)
+open_last_report_button.bind("<Enter>",
+                             lambda a: open_last_report_button.configure(image=open_last_report_image_lighted))
+open_last_report_button.bind("<Leave>", lambda a: open_last_report_button.configure(image=open_last_report_image))
 
+# SETUP TEST RUN button desc
+setup_test_run_image = PhotoImage(file='D:/testing/soft/setup_test_run.gif')
+setup_test_run_image_lighted = PhotoImage(file='D:/testing/soft/setup_test_run_lighted.gif')
+setup_test_run_button = Button(f1, width=button_menu_width, height=button_menu_height, bd=border_value,
+                               fg="white", image=setup_test_run_image,
+                               command=lambda: test2.setup_test_run(), font=button_font)
+setup_test_run_button.place(x=horiz_placemnt * 0.46, y=114)
+setup_test_run_button.bind("<Enter>", lambda a: setup_test_run_button.configure(image=setup_test_run_image_lighted))
+setup_test_run_button.bind("<Leave>", lambda a: setup_test_run_button.configure(image=setup_test_run_image))
+
+# BROWSE YOUR TESTS button desc
+browse_tests_image = PhotoImage(file='D:/testing/soft/browse.gif')
+browse_tests_image_lighted = PhotoImage(file='D:/testing/soft/browse_lighted.gif')
+browse_tests_button = Button(f1, text="Browse Your Tests", height=19, image=browse_tests_image, bd=1,
+                             command=lambda: test2.browsecsv(listbox1, vert_placemnt, horiz_placemnt))
+browse_tests_button.place(x=2, y=vert_placemnt * 0.87, width=horiz_placemnt * 0.4)
+browse_tests_button.bind("<Enter>", lambda a: browse_tests_button.configure(image=browse_tests_image_lighted))
+browse_tests_button.bind("<Leave>", lambda a: browse_tests_button.configure(image=browse_tests_image))
 
 root.mainloop()
--- test2.py
+++ test2.py
@@ -1,49 +1,223 @@
 import subprocess
 from tkinter.filedialog import *
+from tkinter import ttk
 
-global path_to_test
+path_to_test = open('D:/testing/soft/Settings.ini', 'r').readlines()
 
 
-def browsecsv(listbox1, vert_placemnt):
+def browsecsv(listbox1, vert_placemnt, horiz_placemnt):
     global path_to_test
+    listbox1.delete(0, END)
     path_to_test = askdirectory()
+    setting = open('D:/testing/soft/Settings.ini', 'w')
+    setting.write(path_to_test)
     a = os.listdir(path_to_test)
+    setting.close()
     for i in a:
         if i == ".cache" or i == "__pycache__" or i == "rep" or i == "BaseTestCase.py" or i == "ConfigParser.py" or i == "GetPage.py":
             continue
-        else:
+        elif i.endswith('.py'):
             listbox1.insert(END, i)
-
-    listbox1.place(x=0, y=0)
+    listbox1.place(x=2, y=0, height=vert_placemnt * 0.87, width=horiz_placemnt * 0.4)
     scrollbar = Scrollbar(listbox1)
-    scrollbar.place(x=236, y=0, height=vert_placemnt * 0.89)
+    scrollbar.place(x=horiz_placemnt * 0.4 - 19, y=-1, height=vert_placemnt * 0.87)
     scrollbar['command'] = listbox1.yview
     listbox1['yscrollcommand'] = scrollbar.set
 
 
-def center(toplevel):  # центрирует появление окна
+def center(toplevel, window):  # центрирует появление окна
     toplevel.update_idletasks()
     w = toplevel.winfo_screenwidth()
     h = toplevel.winfo_screenheight()
-    horiz_placemnt = w / 3
-    vert_placemnt = h / 3
+    if window == "root":
+        horiz_placemnt = w / 2.98
+        vert_placemnt = h / 2.71
+    elif window == "config":
+        horiz_placemnt = w / 5
+        vert_placemnt = h / 3.9
     toplevel.geometry(
         "%dx%d+%d+%d" % (horiz_placemnt, vert_placemnt, (w - horiz_placemnt) / 2, (h - vert_placemnt) / 2))
     return horiz_placemnt, vert_placemnt
 
 
-def run_test(listbox1):
+def run_test(listbox1, path, run_tests_button, horiz_placemnt):
     global path_to_test
+    run_tests_button.place_forget()
     selected_item = listbox1.get(ACTIVE)
-    cmd = 'cd /d ' + path_to_test + ' & ' + 'py.test ' + selected_item + ' -s --alluredir rep'
-    subprocess.Popen(cmd, shell=True)
+    if path_to_test != '':
+        cmd = 'cd /d ' + path_to_test[0] + ' & ' + 'py.test ' + selected_item + ' -s --alluredir rep'
+    else:
+        cmd = 'cd /d ' + path[0] + ' & ' + 'py.test ' + selected_item + ' -s --alluredir rep'
+    subprocess.Popen(cmd, shell=True).poll()
+    #run_tests_button.place(x=horiz_placemnt * 0.46, y=145)
 
 
 def generate_report():
-    cmd = 'cd /d d:/Automation/bin' + ' & ' + 'allure generate D:/testing/ddt_testing/tests/rep' + ' & ' + 'allure report open'
+    cmd = 'cd /d d:/Automation/bin' + ' & ' + 'allure generate D:/testing/ddt_testing/tests/rep'
     subprocess.Popen(cmd, shell=True)
 
 
 def open_last_report():
     cmd = 'cd /d d:/Automation/bin' + ' & ' + 'allure report open'
     subprocess.Popen(cmd, shell=True)
+
+
+def setup_test_run():
+    config_ini = open('D:/testing/ddt_testing/config.ini', 'r').readlines()
+    config_readed = []
+    launch_parameter = ''
+    counter = 0
+    for i in config_ini[1:]:
+        for k in i:
+            launch_parameter += k
+            if k == ':' and counter == 0:
+                counter = 1
+                launch_parameter = ''
+            if k == '\n':
+                counter = 0
+                config_readed.append(launch_parameter)
+                launch_parameter = ''
+
+    config = Toplevel()
+    center(config, "config")
+    config.deiconify()
+    config.grab_set()
+    config.focus_force()
+    config.title('TIAA Automation')
+    config.resizable(False, False)
+    im = PhotoImage(file='D:/testing/soft/config_image.gif')
+    label1 = Label(config, image=im)
+    label1.pack()
+    main_orange = '#%02x%02x%02x' % (255, 184, 61)
+    font = 'Anago 10 bold'
+
+    # ----------------------------Buttons----------------------------------------------------------------------------
+    ok = Button(config, text="  Save  ", bg=main_orange, fg='white', font=font, bd=0,
+                command=lambda: change_current_settings(link_text, variable, timeout_text, config))
+    ok.place(x=141, y=220)
+    ok.bind("<Enter>", lambda a: ok.configure(bg="burlywood1"))
+    ok.bind("<Leave>", lambda a: ok.configure(bg=main_orange))
+
+    cancel = Button(config, text="Cancel", bg="gray64", fg='white', font=font, bd=0,
+                    command=lambda: close_settings(config))
+    cancel.place(x=195, y=220)
+    cancel.bind("<Enter>", lambda a: cancel.configure(bg="gray79"))
+    cancel.bind("<Leave>", lambda a: cancel.configure(bg="gray64"))
+
+    text_box_color = 'antique white'
+    # Browser textbox
+    browser_text = Text(config, width=15, height=1, bg=text_box_color)
+    browser_text.place(x=200, y=75)
+
+    # -----------------------------------------------------------------------------------------------------------------
+
+    def func():
+        if variable.get() == "Firefox":
+            browser_dropdown.configure(image=firefox)
+        elif variable.get() == "Chrome":
+            browser_dropdown.configure(image=chrome)
+        elif variable.get() == "PhantomJS":
+            browser_dropdown.configure(image=phantom)
+
+    # Browser dropdown
+    browser_list = [
+        "Firefox",
+        "Chrome",
+        "PhantomJS"
+    ]
+
+    variable = StringVar(config)
+    dropdown_image = PhotoImage(file='dropdown_image.png')
+    firefox = PhotoImage(file='dropdown_firefox.png')
+    chrome = PhotoImage(file='chrome_dropdown.png')
+    phantom = PhotoImage(file='phantom_dropdown.png')
+    variable.set("Choose a Browser")
+
+    dropdown_lighted_image = PhotoImage(file='dropdown_lighted.png')
+    dropdown_lighted = Label(config, image=dropdown_lighted_image, bg="bisque", width=124, height=19)
+    browser_dropdown = OptionMenu(config, variable, *browser_list, command=lambda x: func())
+    browser_dropdown.configure(width=120, height=15, bd=0, indicator=0,
+                               image=dropdown_image)  # if bg is picture width=92, height=15
+    browser_dropdown.bind("<Enter>", lambda a: dropdown_lighted.place(x=198, y=73))
+    browser_dropdown.bind("<Leave>", lambda a: dropdown_lighted.place_forget())
+
+    # Set-up browser by default
+    if config_readed[0] == " Firefox\n":
+        variable.set(browser_list[0])
+        browser_dropdown.configure(image=firefox)
+    elif config_readed[0] == " Chrome\n":
+        variable.set(browser_list[1])
+        browser_dropdown.configure(image=chrome)
+    elif config_readed[0] == " PhantomJS\n":
+        variable.set(browser_list[2])
+        browser_dropdown.configure(image=phantom)
+    browser_dropdown.place(x=200, y=75)
+
+    # Link textbox
+    link_text = Text(config, width=17, height=1, bg=text_box_color, font='Anago-Book 10', fg='gray38')
+    link_text.insert(END, config_readed[1])
+    link_text.place(x=200, y=110)
+
+    # Timeout
+    timeout_text = Text(config, width=17, height=1, bg=text_box_color, font='Anago-Book 10', fg='gray38')
+    timeout_text.insert(END, config_readed[3])
+    timeout_text.place(x=200, y=145)
+
+    # --------------------------Tooltips------------------------------------------------------------------------------------
+
+    tooltip_image = PhotoImage(file='tooltip.gif')
+    tooltip1_x = 116
+    tooltip1_y = 66
+    tooltip1 = Label(config, width=18, height=18, bg="white", im=tooltip_image)
+    tooltip1.place(x=tooltip1_x, y=tooltip1_y)
+    tooltip2_x = 153
+    tooltip2_y = 100
+    tooltip2 = Label(config, width=18, height=18, bg="white", im=tooltip_image)
+    tooltip2.place(x=tooltip2_x, y=tooltip2_y)
+    tooltip3_x = 116
+    tooltip3_y = 139
+    tooltip3 = Label(config, width=18, height=18, bg="white", im=tooltip_image)
+    tooltip3.place(x=tooltip3_x, y=tooltip3_y)
+    tooltip4_x = 96
+    tooltip4_y = 174
+    tooltip4 = Label(config, width=18, height=18, bg="white", im=tooltip_image)
+    tooltip4.place(x=tooltip4_x, y=tooltip4_y)
+
+    browser_tooltip_image = PhotoImage(file='tooltip1.png')
+    browser_tooltip = Label(config, width=115, height=20, im=browser_tooltip_image, bd=0)
+    link_tooltip_image = PhotoImage(file='tooltip_link.png')
+    link_tooltip = Label(config, width=129, height=20, im=link_tooltip_image, bd=0)
+    timeout_tooltip_image = PhotoImage(file='tooltip_timeout.png')
+    timeout_tooltip = Label(config, width=220, height=20, im=timeout_tooltip_image, bd=0)
+    timer_tooltip_image = PhotoImage(file='tooltip_timer.png')
+    timer_tooltip = Label(config, width=115, height=20, im=timer_tooltip_image, bd=0)
+
+    tooltip1.bind("<Enter>", lambda a: browser_tooltip.place(x=tooltip1_x + 22, y=tooltip1_y - 15))
+    tooltip1.bind("<Leave>", lambda a: browser_tooltip.place_forget())
+    tooltip2.bind("<Enter>", lambda a: link_tooltip.place(x=tooltip2_x + 22, y=tooltip2_y - 15))
+    tooltip2.bind("<Leave>", lambda a: link_tooltip.place_forget())
+    tooltip3.bind("<Enter>", lambda a: timeout_tooltip.place(x=tooltip3_x + 22, y=tooltip3_y - 15))
+    tooltip3.bind("<Leave>", lambda a: timeout_tooltip.place_forget())
+    tooltip4.bind("<Enter>", lambda a: timer_tooltip.place(x=tooltip4_x + 22, y=tooltip4_y - 15))
+    tooltip4.bind("<Leave>", lambda a: timer_tooltip.place_forget())
+
+    config.mainloop()
+
+
+def change_current_settings(link_text, variable, timeout_text, window):
+    config_open = open('D:/testing/ddt_testing/config.ini', 'w')
+    data = [
+        "[Launch parameters]",
+        "browser: " + variable.get(),
+        "link:" + link_text.get("1.0", 'end-2c'),
+        "expired_link:" + link_text.get("1.0", 'end-2c') + "/Login/logout.php?message=sessionExpired",
+        "implicity_wait:" + timeout_text.get("1.0", 'end-2c')
+    ]
+    for string in data:
+        config_open.write(string + "\n")
+    config_open.close()
+    window.destroy()
+
+
+def close_settings(window):
+    window.destroy()
